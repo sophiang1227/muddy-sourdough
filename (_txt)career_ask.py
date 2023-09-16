49b81e9dc717d0224f644e42c144ee3575b1b5bf
Sophia Ng <61060603+sophiang1227@users.noreply.github.com>
2023-09-17 00:28:16 +0800

    Update (_txt)career_ask.py

--- (_txt)career_ask.py
+++ (_txt)career_ask.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1W0EOc4vvLP0up_6f8tA-gCE4G963chGE
 """
 
-pip install -r requirements.txt
-
 import streamlit as st
 import os
 import sys
